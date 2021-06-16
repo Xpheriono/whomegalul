b89fdb7b6d1d47394ee6304bae8ef6cda01d6ef9
Xpheriono <mja3u@mtmail.mtsu.edu>
2021-06-16 23:47:32 +0000

    re-wired url redirects

--- apps/who/urls.py
+++ apps/who/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import IndexView, ResultsView, SubmitView
+from .views import IndexView, ResultsView, SubmitView, verify_query
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('results/', SubmitView.as_view(), name='submit-view'),
-    path('results/<str:pk>/', ResultsView.as_view(), name='results'),
+    path('results/', verify_query, name='verify'),
+    path('results/<str:login>/', ResultsView.as_view(), name='results'),
 ]
